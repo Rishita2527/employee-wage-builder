d437aa145b71d29a117193d30e5a6f936d94caa1
rishita1234 <rishita123@gmail.com>
2022-11-21 15:23:26 +0530

    Changed: Create new class and object

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 public class EmployeeWageComputation {
 	void checkEmployeeAttendence() {
  	   int IS_FULL_TIME = 1;
@@ -8,9 +11,23 @@ public class EmployeeWageComputation {
  	   else
  		   System.out.println("Employee is Absent");
     }
+	void calculateEmployeeWage(){
+		 int fullTime=1;
+		 int employeeRatePerHrs=20;
+		 int employeeHrs=0;
+		 int employeeWage=0;
+		 double employeeCheck=Math.floor(Math.random()*10)%2;
+		 if(employeeCheck == fullTime)
+			 employeeHrs=8;
+		 else
+			 employeeHrs=0;
+		 employeeWage=employeeHrs*employeeRatePerHrs;
+		 System.out.println("Employee Wage "+employeeWage);
+	}
 	public static void main(String[] args) {
 		EmployeeWageComputation  Emp = new EmployeeWageComputation ();
 		Emp.checkEmployeeAttendence();
+		Emp.calculateEmployeeWage();
 	}
 
 }
