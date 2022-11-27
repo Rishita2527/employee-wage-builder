68860ad8254006085811812134f73426d84a6c1f
rishita1234 <rishita123@gmail.com>
2022-11-27 17:28:49 +0530

    Refactor: create new method for Total wages

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -81,12 +81,40 @@ public class EmployeeWageComputation {
       monthlyWage=employeeWage * workingDaysPerMonth;
       System.out.println("Employee Monthly Wage " + monthlyWage);
   }
+  
+  //Calculate Wages till a condition of total working hours or days is reached for a month
+  void employeeTotalWage() {
+	  	int wagePerHrs = 20;
+	  	int maxWorkingDays=20;
+	  	int maxWorkingHrs=100;
+	  	int totalWage=0;
+	  	int workingHrs=0;
+	  	System.out.printf("%5s %5s %5s %5s\n", "day", "Workinghrs", "Wage", "Total working hrs");
+	  	for (int day=1, totalWorkingHrs=0; day<=maxWorkingDays && totalWorkingHrs < maxWorkingHrs; day++, totalWorkingHrs += workingHrs)
+	  	{
+	  		int empType= (int) Math.floor(Math.random()*10)%3;
+	  	    switch (empType)
+	  	{
+	  	case 1:workingHrs=8;
+    	       break;
+    	case 2:workingHrs=4;
+    	       break;
+    	default:
+    		   workingHrs=0;
+	  	}
+	  	int wage = workingHrs*wagePerHrs;
+	  	totalWage += wage;
+	  	System.out.printf("%5d %5d %5d %5d\n", day,workingHrs, wage, totalWorkingHrs + workingHrs);
+	  	}
+	  	System.out.println("Total wage for a month is" + totalWage);
+  }
 	public static void main(String[] args) {
 		EmployeeWageComputation  Emp = new EmployeeWageComputation ();
 		//Emp.checkEmployeeAttendence();
 		//Emp.calculateEmployeeWage();
 		//Emp.calculateEmployeePartTimeWage();
         //System.out.println(Emp.switchcase());
-		Emp.employeeMonthlyWage();
+		//Emp.employeeMonthlyWage();
+		  Emp.employeeTotalWage();
 	}
 }
